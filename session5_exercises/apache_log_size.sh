773c928aa65688f86c798ac73621cc7deeb913c3
Jonathan Rathjen <jonathanrathjen@yahoo.com>
2025-05-30 09:06:18 +0000

    finishing apache log exercise

---
+++ session5_exercises/apache_log_size.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ size=$(du -h /var/log/httpd/access_log)
+ num=$(echo $size | grep -oP '\d+')
+
+ echo $size | grep K > /dev/null
+ kilo=$(echo $?)
+
+ echo $size | grep M > /dev/null
+ mega=$(echo $?)
+
+ echo $size | grep G > /dev/null
+ giga=$(echo $?)
+
+if [[ $kilo -eq 0 ]]; then
+    echo "Log size is less than 100MB"
+    exit
+elif [[ $mega -eq 0 ]]; then
+    if [[ $num -gt 100 ]]; then
+        tar -czf access_log_$(date +%F).gz /var/log/httpd/access_log
+        : > /var/log/httpd/access_log
+        systemctl restart httpd
+        exit
+    else
+        echo "Log size is less than 100MB"
+        exit
+    fi
+else
+    tar -czf access_log_$(date +%F).gz /var/log/httpd/access_log
+    : > /var/log/httpd/access_log
+    systemctl restart httpd
+fi
